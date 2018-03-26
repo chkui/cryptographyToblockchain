d80c735b8f10ba9abe0c4fc6f152bb7a921b472c
chkui <chenkui@chkui.com>
2018-03-27 01:19:08 +0800

    complete RSA key generate

--- browser/client1/src/client1.js
+++ browser/client1/src/client1.js
@@ -1,5 +1,14 @@
-//DEMO
-/*123123*/
+const BigInteger = require('jsbn').BigInteger
+
 import GenKey from './rsa/key'
-var key = new GenKey(1234567);
-key.generate();
+const key = new GenKey(1234567);
+key.generate('1231231231232312912897312678312','9128973126783126575677645667765','6575677645667765');
+const text = new BigInteger('23522678');
+console.log("Text :", text.toString());
+const sk = key.getSK()
+const pk = key.getPK()
+const enText = text.modPow(sk.e, sk.n);
+console.log("enText :", enText.toString());
+
+const deText = enText.modPow(pk.d, pk.n);
+console.log("M :", deText.toString());
--- browser/client1/src/rsa/key.js
+++ browser/client1/src/rsa/key.js
@@ -1,119 +1,166 @@
+import {info} from '../util/log'
 const BigInteger = require('jsbn').BigInteger
-//JS的最大计算精度为9007199254740991，超过之后所有大数计算将无效
+/*JS的最大计算精度为9007199254740991，超过之后所有大数计算将无效*/
+/**
+ * RSA密钥对
+ */
 
-var Len = new BigInteger('2').pow(new BigInteger('128'))
-var seed = '777777777777777';
-var checkLun = 500;
-function RsaKey(seed) {
-    this.seed = 'number' === typeof seed ? seed : Number(seed)
+/**
+ * 生成种子的最大长度
+ * @type {*|number}
+ * @private
+ */
+const Config_SeedLen = new BigInteger('2').pow(new BigInteger('128')),
+    /**
+     * 密钥或加密数据的最大长度，n的边界
+     * @type {*|number}
+     */
+    Config_KeyLen = new BigInteger('2').pow(new BigInteger('256')),
+    /**
+     * millerRabin算法的检查次数
+     * @type {number}
+     */
+    Config_millerRabinCheckNum = 1500,
+    Config_Step = new BigInteger('1'); //算法的步进长度，
+function RsaKey() {
+    let sk = {k: false, n: false}, pk = {k: false, n: false};
+
+    this.getSK = function () {
+        return sk;
+    };
+    this.getPK = function () {
+        return pk;
+    };
+    this.setSK = function (input) {
+        sk = input;
+    };
+    this.setPK = function (input) {
+        pk = input;
+    };
 }
 
-RsaKey.prototype.generate = function () {
-    var p = new BigInteger(seed),
-        q = new BigInteger(seed),
-        step = new BigInteger('1');
+/**
+ * 生成RSA密钥
+ * @param {string} seedP p的种子
+ * @param {string} seedQ q的种子
+ * @param {string} seedE e的种子
+ */
+RsaKey.prototype.generate = function (seedP, seedQ, seedE) {
+    let p, //素数1
+        q, //素数2
+        n, //p*q
+        fn,//(p-1)(q-1)
+        e, //PK
+        d; //SK
 
-    while (true) {
-        if (p.isProbablePrime(checkLun)) {
-            console.log('Prime :', p.toString())
-            break;
-        } else {
-            p = p.add(step);
-        }
-    }
+    p = genPrime(seedP, 'P');
+    q = genPrime(seedQ, 'Q');
 
-    while (true) {
-        if (q.isProbablePrime(checkLun)) {
-            console.log('Prime :', q.toString())
-            break;
-        } else {
-            q = q.subtract(step);
-        }
-    }
+    n = p.multiply(q).mod(Config_KeyLen);
+    info('n: ', n.toString(), '. length: ', n.bitLength());
+    fn = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE)).mod(Config_KeyLen);
+    info('f(n): ', n.toString(), '. length: ', fn.bitLength());
+    e = genRelativelyPrime(fn, seedE);
 
-    var n = p.multiply(q).mod(Len);
-    var fn = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE)).mod(Len)
+    d = extEuclidean(e, fn);
+    console.log("D :", d.toString());
 
-    console.log('Len :' , fn.bitLength())
+    this.setSK({e,n})
+    this.setPK({d,n})
+};
 
-    console.log('n :', n.toString())
-    console.log('f(n) :', fn.toString())
-    var e = new BigInteger('3');
-
-    while (true) {
-        if (0 === step.compareTo(fn.gcd(e))) {
-            console.log('E :', e.toString())
-            break;
-        } else {
-            e = e.add(BigInteger.ONE);
-        }
-    }
-    var x, y;
-
+/**
+ * 欧几里得扩展算法，用于推导RSA的d
+ * @param e
+ * @param fn
+ */
+function extEuclidean(e, fn) {
+    let x, y, iterCount = 0;
     function ex_gcd(a, b) {
         if (b.intValue() === 0) {
             x = new BigInteger("1");
             y = new BigInteger("0");
             return a;
         }
-        var ans = ex_gcd(b, a.mod(b));
-        var temp = x;
+        const ans = ex_gcd(b, a.mod(b));
+        const temp = x;
         x = y;
         y = temp.subtract(a.divide(b).multiply(y));
+        iterCount++;
         return ans;
     }
-
-    function cal(a, k) {
-        var gcd = ex_gcd(a, k);
-        if (BigInteger.ONE.mod(gcd).intValue() !== 0) {
-            return new BigInteger("-1");
-        }
-        x = x.multiply(BigInteger.ONE.divide(gcd));
-        k = k.abs();
-        var ans = x.mod(k);
-        if (ans.compareTo(BigInteger.ZERO) < 0) ans = ans.add(k);
-        return ans;
+    const gcd = ex_gcd(e, fn);
+    if (BigInteger.ONE.mod(gcd).intValue() !== 0) {
+        return new BigInteger("-1");
     }
-
-    var d = cal(e, fn)
-    console.log("D :", d.toString());
-
-    var pk = {n, e}, sk = {n, d}
-
-
-    var m = new BigInteger('22');
-    console.log("M :", m.toString());
-    var c = m.modPow(e, n);
-    console.log("C :", c.toString());
-
-    var text = c.modPow(d, n);
-    console.log("M :", text.toString());
-
-
-    /*console.log(Math.pow(2, 32));
-    console.log(2 << 32);
-
-    console.log(a);*/
-
-    /*while(true){
-        p++;
-        console.log('p: ', p);
-        if(this.isPrime(p)) break;
-        this.Len < p && (p = 3);
-    }
-    while(true){
-        if(this.isPrime(q--)) break;
-        3 > q && (q = RsaKey.prototype.Len - 3);
-        console.log('q: ', q);
-    }*/
+    x = x.multiply(BigInteger.ONE.divide(gcd));
+    fn = fn.abs();
+    let ans = x.mod(fn);
+    if (ans.compareTo(BigInteger.ZERO) < 0) ans = ans.add(fn);
+    info('Generate D with E=', e.toString(), ', fn=', fn.toString(), '. Result :', ans.toString(), '. Iteration Count:', iterCount);
+    return ans;
 }
-RsaKey.prototype.isPrime = function (n) {
-    if (n < 0x2) return false;
-    if (0x2 === n) return true;
-    if (0x0 === n % 0x2) return false;
-    for (var i = 0x3; i < n; i += 0x2)
-        if (0x0 === n % i) return false;
-    return true;
+
+// private
+/* * 生成素数，采用上线发散式的搜索
+ * @param seed
+ * @param name 日志输出名称
+ * @returns {*}
+ */
+function genPrime(seed, name) {
+    let result, add = new BigInteger(seed).mod(Config_SeedLen), sub = add.clone(), checkCount = 0;
+    while (!(result = checkPrime(add)) || !(result = checkPrime(sub))) {
+        add = add.add(Config_Step)
+        sub = sub.subtract(Config_Step)
+        checkCount++;
+    }
+    info('Generate prime ', name ? name : '', ':', result.toString(), '. Iteration Count:', checkCount);
+    return result;
+}
+
+/**
+ * 检查素数
+ * @param prime
+ * @returns {BigInteger|boolean}
+ */
+function checkPrime(prime) {
+    if (prime.isProbablePrime(Config_millerRabinCheckNum)) {
+        return prime;
+    } else {
+        return false;
+    }
+}
+
+/**
+ * 生成与fn互质的数
+ * @param fn
+ * @param seed
+ * @returns {*}
+ */
+function genRelativelyPrime(fn, seed) {
+    let result, add = new BigInteger(seed).mod(Config_SeedLen), sub = add.clone(), checkCount = 0;
+
+    while (!(result = checkRelativelyPrime(fn, add)) || !(result = checkRelativelyPrime(fn, sub))) {
+        add = add.add(Config_Step)
+        sub = sub.subtract(Config_Step)
+        checkCount++;
+    }
+    info('Generate E(gcd) with ', fn.toString(), ': ', result.toString(), '. Iteration Count:', checkCount);
+    return result;
+}
+
+/**
+ * 检查互质
+ * @param fn
+ * @param num
+ * @returns {*}
+ */
+function checkRelativelyPrime(fn, num) {
+    if (0 === BigInteger.ONE.compareTo(fn.gcd(num))) {
+        return num;
+    } else {
+        return false;
+    }
 }
 
 export default RsaKey
